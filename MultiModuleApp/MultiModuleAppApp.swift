ec1ec914f11a3cbeedee0370faa70a19751f5b0c
medhatmebed <medhat.mebed@gmail.com>
2024-01-13 08:15:50 +0200

    Initial Commit

---
+++ MultiModuleApp/MultiModuleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiModuleAppApp.swift
+//  MultiModuleApp
+//
+//  Created by Medhat Mebed on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiModuleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
